e9dbaad76806e40b82b3222fa40dfb016bf5f154
xuanjiantao742-crypto <xuanjiantao742@gmail.com>
2026-07-22 08:56:04 +0800

    Honor preregistered 2019-10 research start

---
+++ tools/run_fbr_public_data_preregistered.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Apply the locked FBR preregistration start date before invoking the builder."""
+import datetime as dt
+import build_fbr_public_data as builder
+
+builder.START = dt.datetime(2019, 10, 1, tzinfo=dt.timezone.utc)
+raise SystemExit(builder.main())
